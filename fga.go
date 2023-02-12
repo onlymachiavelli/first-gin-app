6182b6c3ed2ce5f1e9a50266afccb67ea91d804a
Alaa Barka <tmakaveli643@gmail.com>
2023-02-12 19:49:19 +0100

    preparing the project

---
+++ fga.go
@@ -0,0 +1,19 @@
+package fga 
+
+
+import (
+	"fmt" 
+	"net/http" 
+	"github.com/gin-gonic/gin"
+)
+func main () {
+
+	app:= gin.Default() 
+	app.GET("/" , func(g * gin.Context) {
+		g.JSON(http.StatusOK, gin.H{
+			"msg" : "Hello world"
+ 		})
+	} )
+
+	app.Run()
+}
